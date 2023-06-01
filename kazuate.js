717df5a69af4e718fceaf0d86e9ec36fadf13c12
G284212022 <228421@st.takushoku-u.ac.jp>
2023-06-01 16:01:29 +0900

    確認

--- kazuate.js
+++ kazuate.js
@@ -18,7 +18,7 @@ b1.addEventListener('click',hantei);
 function hantei() {
   kaisu++
   // 将来ここでは 4 ではなくテキストボックスに指定された数値を yoso に代入する
-  let yoso = 4;
+  let yoso = document.querySelector('input[answer="yoso"]');
   kaisu = kaisu + 1;
   let kai = document.querySelector('span#kaisu');
   kai = kaisu;
